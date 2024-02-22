1bd8f351f2aae6cb9329841952bb59113438774d
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-22 08:45:16 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ~
+sudo apt install make git nano clang pkg-config libssl-dev build-essential -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env
+rustc --version
+git clone https://github.com/availproject/avail-light.git
+cd avail-light
+cargo build --release
+
+sudo tee /etc/systemd/system/availightd.service > /dev/null <<EOF
+[Unit]
+Description=Avail Light Client
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+User=root
+ExecStart=/root/avail-light/target/release/avail-light --network goldberg
+Restart=always
+RestartSec=120
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable availightd
