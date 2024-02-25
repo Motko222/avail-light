e3c8bd922fe02a41ed1e3d70e93ee346aeba6545
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-25 11:55:14 +0100

    Create report.sh

---
+++ report.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+service=$(sudo systemctl status avail-light --no-pager | grep "active (running)" | wc -l)
+
+if [ $service -ne 1 ]
+then 
+  status="error";
+  note="service not running"
+else 
+  status="ok";
+fi
+
+tee << EOF
+{
+  "status"="$status",
+  "service"="$service"
+}
+EOF
