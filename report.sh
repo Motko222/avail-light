6e9815c721413475778604b147669c972aa32d04
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-26 13:58:15 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ~/scripts/avail-light/config/env
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 service=$(sudo systemctl status availightd --no-pager | grep "active (running)" | wc -l)
@@ -12,9 +11,9 @@ fi
 tee | jq << EOF
 {
   "project":"$folder",
-  "id":$ID,
+  "id":$AVAIL_ID,
   "machine":"$MACHINE",
-  "chain":"$CHAIN",
+  "chain":"$AVAIL_CHAIN",
   "status":"$status",
   "note":"$note",
   "service":$service
