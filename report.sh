161353c382b2156eee1446916d4d8a14c15b661b
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-26 09:50:52 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,10 +3,16 @@
 
 service=$(sudo systemctl status availightd --no-pager | grep "active (running)" | wc -l)
 
-[ $service -ne 1 ] && status="error";note="service not running" || status="ok"
+if [ $service -eq 1 ]
+then status="ok"
+else status="error" note="service not running"
+fi
 
 tee | jq << EOF
 {
+  "project":"$project",
+  "id":$id,
+  "machine":"$machine",
   "status":"$status",
   "note":"$note",
   "service":$service
