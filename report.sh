3fbbb3142ef0d70b347e028de46ae14b2bd24a41
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-08 18:12:36 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-source ~/scripts/$folder/conf
+#folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+#source ~/scripts/$folder/conf
+source ~/.bash_profile
 
 service=$(sudo systemctl status availightd --no-pager | grep "active (running)" | wc -l)
+id=avail-$AVAIL_ID
+chain=goldberg
 
 if [ $service -eq 1 ]
 then status="ok"
@@ -13,9 +16,9 @@ fi
 cat << EOF
 {
   "project":"$folder",
-  "id":"$ID",
+  "id":"$id",
   "machine":"$MACHINE",
-  "chain":"goldberg",
+  "chain":"$chain",
   "type":"node",
   "status":"$status",
   "note":"$note",
@@ -23,3 +26,16 @@ cat << EOF
   "updated":"$(date --utc +%FT%TZ)"
 }
 EOF
+
+# send data to influxdb
+if [ ! -z $INFLUX_HOST ]
+then
+ curl --request POST \
+ "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$INFLUX_BUCKET&precision=ns" \
+  --header "Authorization: Token $INFLUX_TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary '
+    status,node='$id',machine='$MACHINE' status="'$status'",message="'$message'",version="'$version'",url="'$url'",chain="'$chain'" '$(date +%s%N)' 
+    '
+fi
